a9fb73b78a0f3cc108053bafbb6e5a868f706a85
Alex Sayers <alex@asayers.com>
2025-12-15 17:32:16 +0900

    fake upstream etc.

--- jetrelay/src/upstream.rs
+++ jetrelay/src/upstream.rs
@@ -5,17 +5,89 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 use tracing::*;
 use wsclient::{Frame, OpCode};
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Copy, Clone)]
 pub struct Timestamp(pub u64 /* epoch micros */);
 
+impl Timestamp {
+    fn now() -> Self {
+        Timestamp(
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_micros()
+                .try_into()
+                .unwrap(),
+        )
+    }
+}
+
 impl std::ops::Sub<Duration> for Timestamp {
     type Output = Timestamp;
     fn sub(self, rhs: Duration) -> Self::Output {
         Timestamp(self.0 - rhs.as_micros() as u64)
+    }
+}
+
+pub fn fake_iter() -> impl Iterator<Item = Result<(Frame, Timestamp)>> {
+    std::iter::repeat_with(|| {
+        std::thread::sleep(Duration::from_millis(1));
+        let ts = Timestamp::now();
+        let frame = Frame::text(&format!(
+            "{{ \"time_us\": {}, \"padding\": \"{:>65000}\" }}",
+            ts.0, ' '
+        ));
+        Ok((frame, ts))
+    })
+}
+
+pub fn jetstream_iter(
+    ws_iter: impl Iterator<Item = std::io::Result<Frame>>,
+) -> impl Iterator<Item = Result<(Frame, Timestamp)>> {
+    ws_iter.filter_map(|frame| match frame {
+        Ok(frame) => {
+            let timestamp = parse_frame(&frame)
+                .with_context(|| format!("{:?}", frame.bytes))
+                .transpose()?;
+            Some(timestamp.map(|ts| (frame, ts)))
+        }
+        Err(e) => {
+            warn!("I/O error while reading from websocket: {e:#}");
+            None
+        }
+    })
+}
+
+pub fn mk_stat_printer() -> impl FnMut(&Frame, Timestamp, u64) {
+    #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
+    struct MsgStats {
+        n_msgs: usize,
+        n_bytes: usize,
+        hash: u64,
+    }
+    let mut last_ts_sec = 0;
+    let mut stats = MsgStats::default();
+    move |frame: &Frame, timestamp: Timestamp, file_len: u64| {
+        let ts_sec = timestamp.0 / 1_000_000;
+        if ts_sec != last_ts_sec {
+            let stats = std::mem::take(&mut stats);
+            info!(
+                "[{last_ts_sec}] {:#x} ({} evs, {} KiB), {} MiB total",
+                stats.hash,
+                stats.n_msgs,
+                stats.n_bytes / 1024,
+                file_len / 1024 / 1024,
+            );
+            last_ts_sec = ts_sec;
+        }
+        stats.n_bytes += frame.bytes.len();
+        stats.n_msgs += 1;
+        for bytes in frame.payload().chunks_exact(8) {
+            stats.hash ^= u64::from_le_bytes(bytes.try_into().unwrap());
+        }
     }
 }
 
@@ -35,21 +107,16 @@ const MAX_RETENTION: Duration = Duration::from_secs(2 * 60);
 pub fn copy_frames_to_file(
     mut file: File,
     file_len: Arc<AtomicU64>,
-    iter: impl Iterator<Item = std::io::Result<Frame>>,
+    iter: impl Iterator<Item = Result<(Frame, Timestamp)>>,
 ) -> Result<()> {
     let _g = info_span!("upstream copier thread").entered();
     info!("Copying data from upstream");
     let mut first_timestamp = Timestamp(0);
-    for frame in iter {
-        let frame = match frame {
-            Ok(x) => x,
-            Err(e) => {
-                warn!("I/O error while reading from websocket: {e:#}");
-                continue;
-            }
-        };
-        match handle_frame(&mut first_timestamp, &mut file, &file_len, frame) {
-            Ok(()) => (),
+    let mut print_stats = crate::upstream::mk_stat_printer();
+    for x in iter {
+        let (frame, ts) = x?;
+        match handle_frame(&mut first_timestamp, &mut file, &file_len, &frame, ts) {
+            Ok(file_len) => print_stats(&frame, ts, file_len),
             Err(e) => warn!("Bad frame: {e:#}"),
         }
     }
@@ -60,19 +127,11 @@ fn handle_frame(
     first_timestamp: &mut Timestamp,
     file: &mut File,
     file_len: &AtomicU64,
-    frame: Frame,
-) -> anyhow::Result<()> {
-    match frame.opcode() {
-        OpCode::Text => (),            // Expected
-        OpCode::Ping => return Ok(()), // Ignore
-        OpCode::Close => bail!("Upstream is shutting us down :-("),
-        OpCode::Binary => bail!("Binary frame: {frame:?}"),
-        x => bail!("Unexpected opcode: {x:?}"),
-    }
-    let timestamp = parse_frame(&frame).with_context(|| format!("{:?}", frame.bytes))?;
-
+    frame: &Frame,
+    timestamp: Timestamp,
+) -> Result<u64> {
     file.write_all(&frame.bytes)?;
-    file.flush()?;
+    // file.flush()?;
     let n = frame.bytes.len() as u64;
     trace!("Wrote {n} bytes");
     let offset = file_len.fetch_add(n, Ordering::Release);
@@ -91,20 +150,27 @@ fn handle_frame(
     }
 
     // We could wake up the io_uring here... but we don't bother
-    Ok(())
+    Ok(offset)
 }
 
-fn parse_frame(frame: &Frame) -> anyhow::Result<Timestamp> {
+fn parse_frame(frame: &Frame) -> Result<Option<Timestamp>> {
+    match frame.opcode() {
+        OpCode::Text => (),              // Expected
+        OpCode::Ping => return Ok(None), // Ignore
+        OpCode::Close => bail!("Upstream is shutting us down :-("),
+        OpCode::Binary => bail!("Binary frame: {frame:?}"),
+        x => bail!("Unexpected opcode: {x:?}"),
+    }
     ensure!(frame.reserved_bits() == 0, "Non-zero reserved bits");
     ensure!(frame.mask().is_none(), "Frame is masked");
     let payload = std::str::from_utf8(frame.payload())?;
     let timestamp = gjson::get(payload, "time_us");
     ensure!(timestamp.kind() == gjson::Kind::Number);
     let timestamp = Timestamp(timestamp.u64());
-    Ok(timestamp)
+    Ok(Some(timestamp))
 }
 
-fn drop_old_data(file: &File, ts: Timestamp) -> anyhow::Result<()> {
+fn drop_old_data(file: &File, ts: Timestamp) -> Result<()> {
     static LAST_DROP_OFFSET: AtomicU64 = AtomicU64::new(0);
 
     let mut index = INDEX.lock().unwrap();
