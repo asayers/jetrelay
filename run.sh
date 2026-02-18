2b599a0c00e720242b40a44f11a06ad199c5ad66
Alex Sayers <alex@asayers.com>
2026-02-18 14:55:57 +0900

    Add a systemd-run wrapper script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+bin=$1; shift
+cargo build --release --bin $bin
+systemd-run --user \
+    --pty --same-dir --wait --collect --service-type=exec \
+    -p LimitNOFILE=100000 \
+    -p CPUQuota=100% \
+    -p CPUWeight=10000 \
+    -p AllowedCPUs=1 \
+    -p LimitMEMLOCK=infinity \
+    --unit $bin \
+    target/release/$bin
